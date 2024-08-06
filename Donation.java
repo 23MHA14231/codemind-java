59374d0bb09f5fb78efeb9b8e4d4a8431d000abb
23MHA14231 <147490207+23MHA14231@users.noreply.github.com>
2024-08-07 00:01:24 +0530

    import java.util.Scanner; public class Main {     public static void main(String[] args)     {         Scanner scanner = new Scanner(System.in);         int X = scanner.nextInt();         int Y = scanner.nextInt();         int C = Y-X;             System.out.println(C);
        }
    }

---
+++ Donation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int Y = scanner.nextInt();
+        int C = Y-X;
+            System.out.println(C);
+       
+    }
+}
